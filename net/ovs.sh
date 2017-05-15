fc5b2db3dd0896fe89132c04326e986e98728c03
guoweikang <762666614@.com>
2017-05-15 04:16:04 -0400

    add net

--- net/ovs.sh
+++ net/ovs.sh
@@ -1,9 +1,15 @@
-ovs-vsctl add-br br0
-ovs-vsctl add-port br0 gre1 -- set interface gre1 type=gre option:remote_ip=YOUR_ip 
-brctl addif docker0 br0 
-ip link set dev br0 up 
-ip link set dev docker0 up 
-ip route add 172.17.0.0/16 dev docker0
-iptables -t nat -F 
-iptables -F 
+ovs-vsctl add-br ovs0
+brctl addif docker0 ovs0 
+ifconfig ovs0 up
+
+ovs-vsctl add-port ovs0 gre0 --  set interface gre0 type=gre options:remote_ip=10.10.1.10
+
+route add -net 172.17.0.0/16 dev docker0  
+
+
+
+
+
+
+
 
